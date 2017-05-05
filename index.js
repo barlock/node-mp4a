b8885cf02702c6e77d5e8bc42069230983e4c206
Michael Barlock <mbarlock@us.ibm.com>
2017-05-04 21:05:55 -0400

    Add support for skipping already converted files

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
 "use strict";
 
 const async = require("async"),
-    chalk = require("chalk"),
     glob = require("glob"),
     argv = require("yargs").argv,
-    ffmpeg = require("fluent-ffmpeg"),
-    fs = require("fs"),
     path = require("path"),
     status = require("node-status"),
-    prettyMs = require("pretty-ms"),
-    preset = require("./lib/preset"),
     console = status.console(),
-    globPath = path.resolve(__dirname, argv._[0]),
-    subExtentions = [
-        "srt", "sub", "ssa", "ass", "stl", "pjs", "jss", "rt", "smi"
-    ],
-    pattern = "{uptime.green} {spinner.cyan} Converted: {convert.custom} | {convert.percentage} {convert.bar}";
-
-// Only supports mkv right now
-// ignores image based subtitles
+    mediaConverter = require("./lib/media-converter"),
+    globPath = path.resolve(__dirname, argv._[0]);
 
 /**
  * Given: A non mp4
@@ -46,66 +35,23 @@ const async = require("async"),
  *   Then: create an h.264 version?
  */
 
-let completed = 0, matches = 0;
 
-function convertMedia(media, done) {
-    const basename = path.basename(media),
-        ext = path.extname(media),
-        output = media.replace(ext, ".mp4"),
-        start = Date.now();
-
-    async.waterfall([
-        next => ffmpeg.ffprobe(media, next),
-        (meta, next) => {
-            let progress = {};
-
-            const convertStatus = status.addItem("convert", {
-                label: basename,
-                custom: () => `${completed}/${matches} | ${chalk.magenta(basename)} | ${progress.currentKbps}kb/s | ${progress.timemark}`,
-                max: 100
-            });
-
-            ffmpeg(media)
-                .preset(preset(meta))
-                // .on("start", cli => console.log(cli + "\n"))
-                .on("progress", prog => {
-                    progress = prog;
-                    convertStatus.inc(prog.percent - convertStatus.val);
-                })
-                .on("end", () => next())
-                .on('error', (err) => next(err))
-                .save(output);
-        },
-        next => fs.access(output, fs.constants.F_OK, next),
-        next => fs.unlink(media, next)
-    ], err => {
-        completed += 1;
-
-        if (err) {
-            console.log(`${chalk.red("✖")} Error processing ${basename} ${err.message}`);
-
-            return done(err);
-        }
-
-        console.log(`${chalk.green("✔")} ${prettyMs(Date.now() - start)} | Processed ${basename}`);
-
-        done();
-    });
-}
 
 async.waterfall([
     next => glob(globPath, next),
     (files, next) => {
-        matches = files.length;
+        console.log(`Processing ${files.length} files`);
 
-        status.start({ pattern });
+        const converter = mediaConverter(status, files.length);
+        
+        status.start();
 
         async.eachSeries(files, (file, cb) => {
-            convertMedia(file, cb);
+            converter(file, cb);
         }, next);
     }
 ], () => {
     status.stop();
 
-    console.log(`🎉 Finished processing ${completed} files`);
+    console.log(`🎉 Finished processing files`);
 });
