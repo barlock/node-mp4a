59f975aec5578fe8d66e76e10d215c04afcbc236
Michael Barlock <mbarlock@us.ibm.com>
2017-05-13 12:36:37 -0400

    Better output

--- index.js
+++ index.js
@@ -1,59 +1,7 @@
 "use strict";
 
-const async = require("async"),
-    glob = require("glob"),
-    argv = require("yargs").argv,
-    path = require("path"),
-    status = require("node-status"),
-    console = status.console(),
-    mediaConverter = require("./lib/media-converter"),
-    globPath = path.resolve(__dirname, process.env.DIRECTORY || argv._[0], "**/*.*"),
-    supportedExts = [ ".mp4", ".avi", ".mkv" ];
+const argv = require("yargs").argv,
+    processFiles = require("./lib/process-files"),
+    directory =  process.env.DIRECTORY || argv._[0];
 
-/**
- * Given: A non mp4
- *   When: Converting to an mp4
- *   Then: Copy over all english streams
- *
- * Given: A non mp4
- *   When: Converting to an mp4 without any recognizable english streams
- *   Then: Copy over all streams
- *
- * Given: An mp4
- *   When: There isn't a 2c aac audio track
- *   Then: convert the best eng track to be a 2c aac track
- *
- * Given: An mp4
- *   When: There is a 2c aac audio track
- *   Then: convert the track to be aac
- *
- * Given: An srt
- *   When: matching an mp4
- *   Then: convert to mov_text and embed in the video
- *
- * Given: An mp4
- *   When: the video codec is non h.264
- *   Then: create an h.264 version?
- */
-
-async.waterfall([
-    next => glob(globPath, next),
-    (files, next) => {
-        files = files.filter(file => {
-            return supportedExts.includes(path.extname(file));
-        });
-
-        console.log(`Processing ${files.length} files`);
-
-        const converter = mediaConverter(status, files.length);
-        
-        status.start();
-
-        async.eachSeries(files, (file, cb) => {
-            converter(file, cb);
-        }, next);
-    }
-], () => {
-    status.stop();
-    console.log(`🎉 Finished processing files`);
-});
+processFiles(directory);
