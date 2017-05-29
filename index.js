f9c24407e5a34159b1c3ab5dccae0a26d56cc1c8
Michael Barlock <mbarlock@us.ibm.com>
2017-05-29 18:15:10 -0400

    Add support for converting specific unsupported formats to h264 and cron

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 "use strict";
 
 const argv = require("yargs").argv,
+    schedule = require("node-schedule"),
     processFiles = require("./lib/process-files"),
+    cron = process.env.CRON || argv.cron,
     directory =  process.env.DIRECTORY || argv._[0];
 
-processFiles(directory);
+if (cron) {
+    schedule.scheduleJob(cron, () => processFiles(directory));
+} else {
+    processFiles(directory);
+}
+
+
